0898049ee3565aaaf97b253ac89f8ed1e24479c0
Eric <ericmuthuipatch22@gmail.com>
2023-09-18 20:25:08 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
